20e2fe0129c259723cc6c7495f8740e5867d98d2
Chaitanya Varshney <chaitanya.v@binmile.com>
2023-11-17 00:24:15 +0530

    loginsetup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getToken } from "firebase/messaging";
 import { messaging, app } from "./firebase";
 import Signup from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 
+const auth = getAuth(app)
 
 function App() {
 
 
+
+  useEffect(() =>{
+    onAuthStateChanged(auth, user=>{
+      if(user){
+        // logged in 
+        console.log(`hello ${user}`)
+        setUser(user)
+      }
+      else{
+        // no logged in user
+        console.log(`you are logged out`)
+        setUser(null)
+      }
+    })
+  },[])
+  
+  const [user,setUser] = useState(null);
+
   async function requestPermission() {
+
+
+
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
       // generate token
@@ -28,10 +51,19 @@ function App() {
     // req user for notification permission
     requestPermission();
   }, []);
+
+  if(user===null){
   return <div className="App">
     <Signup/>
     <Signin/>
   </div>;
+}else{
+  return<div>
+    <h1>{user.email}</h1>
+    <button onClick={()=>signOut(auth)}>LOGOUT</button>
+  </div>
+}
+
 }
 
 export default App;
